aafefea2f74917d130c85d22337f4c88acb439f9
xXsheroXx <shero010807@gmail.com>
2024-11-28 14:40:10 +0100

    two services comunicating using Rest Template and http request

---
+++ customer/src/main/java/com/amigoscode/customer/CustomerController.java
@@ -0,0 +1,22 @@
+package com.amigoscode.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "api/v1/customers")
+@AllArgsConstructor
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @PostMapping
+    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
+        log.info("New customer registration: {}", customerRegistrationRequest);
+        customerService.registerCustomer(customerRegistrationRequest);
+    }
+}
